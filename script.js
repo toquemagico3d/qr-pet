e1a2dbe6cb785902ed5a6c04df783bb05fcd2e33
Toque Mágico 3D <pequenosmestres.ff@gmail.com>
2026-03-16 23:52:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function salvarPet(){
 
+let nome = document.getElementById("nome").value
+let tutor = document.getElementById("tutor").value
+let telefone = document.getElementById("telefone").value
+let foto = document.getElementById("foto").value
+
+let id = Date.now()
+
+let pet = {
+id,
+nome,
+tutor,
+telefone,
+foto
+}
+
+localStorage.setItem("pet_"+id,JSON.stringify(pet))
+
+alert("Pet cadastrado!")
+
+let link = "pet.html?id="+id
+
+window.location = link
+
+}
